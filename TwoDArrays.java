8c70c061ed5c8875ca65a05fa1e8ef5604ff6f2e
Nicholas Harasty <Nicholas Harasty>
2017-04-14 10:28:35 -0400

    new file

---
+++ TwoDArrays.java
@@ -0,0 +1,21 @@
+public class TwoDArrays
+{
+	
+	public static void main(String[] args)
+	{
+		int[] list = new list[50];
+		int[][] table = new int[3][5];
+
+		table[0][4] = 7;
+		tabe[1][2] = 9;
+
+		for (int i = 0; i < table.length; i++)
+		{
+			for (int j = 0; j < table[0].length; j++)
+			{
+				System.out.println(j)
+			}
+		}
+	}
+
+}
